81403dd46b68f028df2d1a97b932b52738c66fcd
Dahamboy <125718683+Dahamboy@users.noreply.github.com>
2025-03-06 19:49:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botOwner: 'MR_DPC', // Your name
   ownerNumber: '94783757601', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
