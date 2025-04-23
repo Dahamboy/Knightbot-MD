abaf777030b25d6e58ee5a0c5efb79922c4fb24e
Dahamboy <125718683+Dahamboy@users.noreply.github.com>
2025-04-23 19:36:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'MR DaHAm', // Your name
-  ownerNumber: '94783757601', //Your number
+  ownerNumber: '+94 78 357 6429', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
